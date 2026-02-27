e5c9c0d953881ef2d1eb40c409c30b4ec50bcca9
undeadliner <wemonpf@proton.me>
2026-02-27 21:05:05 +0100

    refactor: rewrite tests with Page Objects and network helpers per README

--- e2e/github-content/list-content.spec.ts
+++ e2e/github-content/list-content.spec.ts
@@ -1,33 +1,29 @@
 import { expect, test } from '@playwright/test'
+import { ContentPage } from '../pages/ContentPage'
+import { waitForNetworkIdle } from '../helpers/network'
 
 test.describe('GitHub Content - List', () => {
+  let contentPage: ContentPage
+
   test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:3000/content/blog')
+    contentPage = new ContentPage(page)
+    await contentPage.navigate('blog')
   })
 
-  test('should display content list', async ({ page }) => {
-    await expect(page.getByRole('heading', { name: /blog/i })).toBeVisible()
-
-    const contentList = page.locator('[data-testid="content-list"]')
-    await expect(contentList).toBeVisible()
+  test('should display content list', async () => {
+    await contentPage.expectToBeVisible()
+    await contentPage.expectItemCount(1)
   })
 
-  test('should filter content by language', async ({ page }) => {
-    const languageSelector = page.locator('[data-testid="language-selector"]')
-    await expect(languageSelector).toBeVisible()
-
-    await page.click('button[data-lang="ru"]')
-
-    const items = page.locator('[data-testid="content-item"]')
-    await expect(items.first()).toContainText('ru')
+  test('should filter content by language', async () => {
+    await contentPage.selectLanguage('ru')
+    await contentPage.expectItemCount(1)
+    await contentPage.expectItemWithTitle('Prometheus')
   })
 
   test('should select content item', async ({ page }) => {
-    const firstItem = page.locator('[data-testid="content-item"]').first()
-    await firstItem.click()
-
-    await expect(firstItem).toHaveClass(/selected/)
-
+    await contentPage.selectItem('Prometheus')
+    
     const editor = page.locator('[data-testid="markdown-editor"]')
     await expect(editor).toBeVisible()
   })
@@ -40,6 +36,7 @@ test.describe('GitHub Content - List', () => {
 
   test('should navigate between content types', async ({ page }) => {
     await page.click('a[href="/content/pages"]')
+    await waitForNetworkIdle(page)
     await expect(page).toHaveURL('/content/pages')
     await expect(page.getByRole('heading', { name: /pages/i })).toBeVisible()
   })
